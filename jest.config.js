7405ef52661a24121cc186d978cce81696a37a3b
Min Huang <min.huang@alumni.usc.edu>
2022-05-15 20:59:18 +0000

    sets up jest perfectly

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  modulePaths: ['<rootDir>'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '.+.ts': 'ts-jest'
+  }
+}
--- tests/hello.test.ts
+++ tests/hello.test.ts
@@ -1,6 +1,6 @@
 import { HELLO_WORLD } from 'src/index'
 
-describe('hello', async () => {
+describe('hello', () => {
   test.concurrent('hello', async () => {
     expect(HELLO_WORLD).toBe('Hello World!')
   })
