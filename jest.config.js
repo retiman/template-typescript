05b116667915c6468139940a2b0ed58e5085f559
Min Huang <min.huang@alumni.usc.edu>
2022-05-15 21:01:07 +0000

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+// See https://jestjs.io/docs/configuration
 module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
   modulePaths: ['<rootDir>'],
   testMatch: ['**/*.test.ts'],
   transform: {
