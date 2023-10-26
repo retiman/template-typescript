a2e9345eb82192103b7e0b354d82527ac6591428
Min Huang <min.huang@alumni.usc.edu>
2023-10-26 15:14:29 -0700

    Updates eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,35 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: ['standard-with-typescript'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    // Disables rules that are handled by prettier already.
+    //
+    // See https://github.com/prettier/eslint-config-prettier
+    'prettier'
+  ],
   parserOptions: {
-    ecmaVersion: 'latest',
     project: './tsconfig.json',
-    sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'jest', 'prettier'],
+  plugins: ['jest'],
   rules: {
     '@typescript-eslint/comma-dangle': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/member-delimiter-style': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/semi': 'off',
-    '@typescript-eslint/space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'always',
-        named: 'never',
-        asyncArrow: 'always',
-      },
-    ],
+    // Disabled because this rule cannot detect if a dependency is only for development.  For example, if you have a
+    // postbuild.js and you include it in tsconfig.json, then this plugin will incorrectly assume that any packages
+    // required there should be in dependencies.
+    //
+    // In that situation, postbuild.js is included in tsconfig.json so that JavaScript files (even ones not included
+    // in the final build) should be linted according to @typescript-eslint.
+    //
+    // See https://stackoverflow.com/questions/61956555/why-is-typescript-eslint-parser-including-files-outside-of-those-configured-in
+    // See https://www.typescriptlang.org/tsconfig#include
+    'import/no-extraneous-dependencies': 'off',
+    // See https://blog.neufund.org/why-we-have-banned-default-exports-and-you-should-do-the-same-d51fdc2cf2ad
+    'import/prefer-default-export': 'off',
   },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.ts'],
-        moduleDirectory: ['node_modules', 'src', 'test/shared', 'test/unit', 'test/integration'],
-      },
-    },
-  },
+  ignorePatterns: [
+    "build",
+    "coverage",
+    "dist",
+    "node_modules"
+  ]
 };
