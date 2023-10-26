30349363ed4e57718c60e5848c14f2e57d09e484
Min Huang <min.huang@alumni.usc.edu>
2023-10-26 15:42:23 -0700

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,19 @@ module.exports = {
   },
   plugins: ['jest'],
   rules: {
-    '@typescript-eslint/comma-dangle': 'off',
+    // Enabled because one should not assign the result of a void function.
+    //
+    // See https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-confusing-void-expression.md
+    '@typescript-eslint/no-confusing-void-expression': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        vars: 'all',
+        varsIgnorePattern: '^_'
+      }
+    ],
     // Disabled because this rule cannot detect if a dependency is only for development.  For example, if you have a
     // postbuild.js and you include it in tsconfig.json, then this plugin will incorrectly assume that any packages
     // required there should be in dependencies.
@@ -23,8 +35,15 @@ module.exports = {
     // See https://stackoverflow.com/questions/61956555/why-is-typescript-eslint-parser-including-files-outside-of-those-configured-in
     // See https://www.typescriptlang.org/tsconfig#include
     'import/no-extraneous-dependencies': 'off',
+    // Disabled because named exports are more explicit.
+    //
+    // See https://github.com/airbnb/javascript/issues/1365
     // See https://blog.neufund.org/why-we-have-banned-default-exports-and-you-should-do-the-same-d51fdc2cf2ad
     'import/prefer-default-export': 'off',
+    // Disabled because we do want to have underscore prefixed identifiers to indicate a variable is ignored.
+    //
+    // See https://eslint.org/docs/latest/rules/no-underscore-dangle
+    'no-underscore-dangle': 'off'
   },
   ignorePatterns: [
     "build",
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,14 @@ export const HELLO_WORLD = 'Hello World!';
 export function foo() {
   return HELLO_WORLD;
 }
+
+// Test for @typescript-eslint/no-unused-vars.
+//
+// Any variable with _ should be exempted from being detected as an unused var.
+export function eslintNoUnusedVars(_: string) {
+  try {
+    // eslint-disable-next-line no-empty
+  } catch (_e) {
+    // eslint-disable-next-line no-empty
+  }
+}
